789d2b8c7c6f51b8d785ac9186971b9292c61684
Travis Fisher <traviswfisher@gmail.com>
2024-06-28 22:52:43 -0400

    adjust makefile for directory structure

--- make.bat
+++ make.bat
@@ -1,7 +1,8 @@
-cl65 -t vic20 -C doom_cc65.cfg -O -l vicdoom.c menu.c automap.c p_enemy.c summary.c victory.c util.c cheatsAsm.s utilAsm.s enemy.s m_random.s doomlogo.s drawLine.s updateInput.s doomfont.s textures.s dpsounds.s playSound.s drawColumnAsm.s logMathAsm.s mapAsm.s -vm -m map.txt -o doom_unc.prg
+cl65 --include-dir c:\retro\cc65-2.13.3\include --lib-path c:\retro\cc65-2.13.3\lib -t vic20 -C doom_cc65.cfg -O -l vicdoom.c menu.c automap.c p_enemy.c summary.c victory.c util.c cheatsAsm.s utilAsm.s enemy.s m_random.s doomlogo.s drawLine.s updateInput.s doomfont.s textures.s dpsounds.s playSound.s drawColumnAsm.s logMathAsm.s mapAsm.s -vm -m map.txt -o doom_unc.prg
 rem \app\pucrunch\pucrunch -c20 doom_unc.prg doom.prg
-\app\exomizer\win32\exomizer sfx basic -t52 -odoom.prg doom_unc.prg
-@echo off
+rem\app\exomizer\win32\exomizer sfx basic -t52 -odoom.prg doom_unc.prg
+exomizer sfx basic -t52 -odoom.prg doom_unc.prg
+rem @echo off
 cc65mapsort map.txt sortedmap.txt labels.txt
 mid2vic\debug\prepend_load_address e1m1 ae00
 mid2vic\debug\prepend_load_address e1m2 ae00
@@ -34,5 +35,5 @@ mid2vic\debug\prepend_load_address victory2.txt be00
 mid2vic\debug\prepend_load_address credits.txt be00
 mid2vic\debug\prepend_load_address order.txt be00
 mid2vic\debug\prepend_load_address help.txt be00
-c:\app\vice\c1541 -format doom,id d64 doom.d64 -write boot -write doom.prg -write ptextures -write pe1m1 -write pe1m2 -write pe1m3 -write pe1m4 -write pe1m5 -write pe1m6 -write pe1m7 -write pe1m8 -write pe1m9 -write pe1m1mus -write pe1m2mus -write pe1m3mus -write pe1m4mus -write pe1m5mus -write pe1m6mus -write pe1m7mus -write pe1m8mus -write pe1m9mus -write pintermus -write pvictormus -write psluts -write psounds -write plowcode -write phicode -write pstackcode -write phelp.txt -write pvictory1.txt -write pvictory2.txt -write pcredits.txt -write porder.txt
-c:\app\vice\xvic doom.d64
+c1541 -format doom,id d64 doom.d64 -write boot -write doom.prg -write ptextures -write pe1m1 -write pe1m2 -write pe1m3 -write pe1m4 -write pe1m5 -write pe1m6 -write pe1m7 -write pe1m8 -write pe1m9 -write pe1m1mus -write pe1m2mus -write pe1m3mus -write pe1m4mus -write pe1m5mus -write pe1m6mus -write pe1m7mus -write pe1m8mus -write pe1m9mus -write pintermus -write pvictormus -write psluts -write psounds -write plowcode -write phicode -write pstackcode -write phelp.txt -write pvictory1.txt -write pvictory2.txt -write pcredits.txt -write porder.txt
+xvic doom.d64
