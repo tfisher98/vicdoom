464c9f84f7c83165d5ace11fbf6bb839d853c3af
Travis Fisher <traviswfisher@gmail.com>
2024-07-06 12:10:19 -0400

    textures to own directory

---
+++ src/textures/textures.s
@@ -0,0 +1,30 @@
+.segment "TEXTURES"
+
+.include "src/textures/PANEL.S"
+.include "src/textures/SEWERWALL.S"
+.include "src/textures/TECHWALL.S"
+.include "src/textures/TILE.S"
+.include "src/textures/GRATE.S"
+.include "src/textures/SWITCH.S"
+.include "src/textures/DOOR.S"
+.include "src/textures/DOORTRAK.S"
+
+.include "src/textures/POSWALK.S"  ;8
+.include "src/textures/POSATK.S"
+.include "src/textures/POSPAIN.S"
+.include "src/textures/IMP.S"      ;11
+.include "src/textures/IMPATK.S"
+.include "src/textures/IMPPAIN.S"
+.include "src/textures/DEMON.S"    ;14
+.include "src/textures/DEMONATK.S"
+.include "src/textures/DEMONPAIN.S"
+.include "src/textures/CACO.S"     ;17
+.include "src/textures/CACOPAIN.S"
+.include "src/textures/CACODEAD.S"
+.include "src/textures/CORPSES.S"  ;20
+
+.include "src/textures/TECHCOLUMN.S"    ;21
+.include "src/textures/ARMOR.S"
+.include "src/textures/ITEMS.S"
+.include "src/textures/BARRELSKULLS.S"
+.include "src/textures/BARXPLD.S"
