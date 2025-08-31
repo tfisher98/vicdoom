32be382d8ed5dcfbbff85246519871a416b2d142
Travis Fisher <traviswfisher@gmail.com>
2025-08-31 00:33:22 -0400

    reformat source code

--- src/display_screen.s
+++ src/display_screen.s
@@ -4,18 +4,18 @@
 .import addysp
 .importzp sp
 
-.export _eraseMessage
-.export _meltScreen
-.export _setTextColor
-.export _print3DigitNumToScreen
-.export _print2DigitNumToScreen
 .export _clearScreen
-.export _setupBitmap
+.export _meltScreen
 .export _clearMenuArea
+.export _setupBitmap
 .export _drawBorders
+.export _eraseMessage
 .export _colorFace
 .export _drawFace
 .export _updateFace
+.export _setTextColor
+.export _print3DigitNumToScreen
+.export _print2DigitNumToScreen
 	
 .autoimport on
 
@@ -34,152 +34,139 @@ bitmapcols = 8
 bitmapcharstart := charscreenstart + charrowsize*2 + charrowsize/2 -bitmapcols/2
 bitmapcolorstart := charcolorstart + charrowsize*2 + charrowsize/2 -bitmapcols/2
 firstbitmapchar = 64
-	
-_eraseMessage:
 
-ldx #40
-lda #32
+.segment "CODE"
+.proc _eraseMessage: near
+    ldx #40
+    lda #32
 woop:
-sta message1,x
-sta message2,x
-dex
-bpl woop
-rts
+    sta message1,x
+    sta message2,x
+    dex
+    bpl woop
+    rts
+.endproc
 
-yyy:
-.byte 0
-yyyx22:
-.byte 0
-xxx:
-.byte 0
-charColor:
-.byte 0
+yyy:       .byte 0
+yyyx22:    .byte 0
+xxx:       .byte 0
+charColor: .byte 0
 
 .segment "HICODE"
-
-_clearScreen:
-
-ldy #0
+.proc _clearScreen: near
+    ldy #0
 :
-  lda #32
-  sta charscreenstart+0,y ; clear screen
-  sta charscreenstart+$100,y
-  sta charscreenstart+$200,y 
-  sta charscreenstart+$300,y
-  lda #2
-  sta charcolorstart,y
-  sta charcolorstart+$100,y
-  sta charcolorstart+$200,y
-  sta charcolorstart+$300,y
-  iny
-  bne :-
-  rts
+    lda #32
+    sta charscreenstart+0,y ; clear screen
+    sta charscreenstart+$100,y
+    sta charscreenstart+$200,y 
+    sta charscreenstart+$300,y
+    lda #2
+    sta charcolorstart,y
+    sta charcolorstart+$100,y
+    sta charcolorstart+$200,y
+    sta charcolorstart+$300,y
+    iny
+    bne :-
+    rts
+.endproc
 
-_setupBitmap:
+.segment "HICODE"
+.proc _setupBitmap: near
+    sta charColor
+    jsr _clearSecondBuffer
+    jsr _copyToPrimaryBuffer
 
-sta charColor
+    ; write an 8x8 block for the graphics
+    ; into the middle of the screen
 
-  jsr _clearSecondBuffer
-  jsr _copyToPrimaryBuffer
-
-; write an 8x8 block for the graphics
-; into the middle of the screen
-
-lda #<bitmapcharstart
-sta bitmapwritechar+1
-sta bitmapwritecolor+1
-lda #>bitmapcharstart	
-sta bitmapwritechar+2
-lda #>bitmapcolorstart
-sta bitmapwritecolor+2
+    lda #<bitmapcharstart
+    sta bitmapwritechar+1
+    sta bitmapwritecolor+1
+    lda #>bitmapcharstart	
+    sta bitmapwritechar+2
+    lda #>bitmapcolorstart
+    sta bitmapwritecolor+2
 	
-lda #0
-sta yyy	
+    lda #0
+    sta yyy	
 
 outerloop:
-;;  start with yyy in A
-clc
-adc #64 			; first bitmap char
-ldx #0
+    ;;  start with yyy in A
+    clc
+    adc #64 			; first bitmap char
+    ldx #0
 innerloop:
-
 bitmapwritechar:
-sta bitmapcharstart,x		; selfmod address
+    sta bitmapcharstart,x		; selfmod address
 
-tay
-lda charColor	
-bitmapwritecolor:
-sta bitmapcolorstart,x 		; selfmod address
-tya
-	
-clc
-adc #8 				; bitmaprows
-inx
-cpx #8 				; bitmapcols
-bne innerloop
+    tay
+    lda charColor	
+    bitmapwritecolor:
+    sta bitmapcolorstart,x 		; selfmod address
+    tya
+    clc
+    adc #8 				; bitmaprows
+    inx
+    cpx #8 				; bitmapcols
+    bne innerloop
 
-lda bitmapwritechar+1
-clc
-adc #40
-sta bitmapwritechar+1
-sta bitmapwritecolor+1
-bcc :+
-inc bitmapwritechar+2
-inc bitmapwritecolor+2
+    lda bitmapwritechar+1
+    clc
+    adc #40
+    sta bitmapwritechar+1
+    sta bitmapwritecolor+1
+    bcc :+
+    inc bitmapwritechar+2
+    inc bitmapwritecolor+2
 :
-inc yyy
-lda yyy
-cmp #bitmaprows
-bne outerloop
-
-rts
+    inc yyy
+    lda yyy
+    cmp #bitmaprows
+    bne outerloop
+    rts
+.endproc
 
 .segment "LOWCODE2"
-_clearMenuArea:
-
-lda #32
-ldx #charrowsize*5-1
+.proc _clearMenuArea: near
+    lda #32
+    ldx #charrowsize*5-1
 :
-sta menustart,x
-dex
-bpl :-
-rts
+    sta menustart,x
+    dex
+    bpl :-
+    rts
+.endproc
 
-_drawBorders:
-rts
+.segment "LOWCODE2"
+.proc _drawBorders: near
+    rts
+.endproc
 
-textcolor:
-.byte 1
-
-_setTextColor:
-  sta textcolor
-  rts
+.segment "LOWCODE2"
+textcolor: .byte 1
+.proc _setTextColor: near
+    sta textcolor
+    rts
+.endproc
 
 .segment "HICODE"
-x40table:
-.byte 0, 40, 80, 120, 160, 200, 240
-meltCount:
-.byte 0
-column:
-.byte 0
+x40table:  .byte 0, 40, 80, 120, 160, 200, 240
+meltCount: .byte 0
+column:    .byte 0
 
 .proc _meltScreen: near
-
     sta sm+1
-   
     lda #180
     sta meltCount
-
 again:
     jsr _P_Random
     lsr
     lsr
     and #7
     sta column
-   
     lda #1
     jsr _waitForRaster
-   
     ; melt column
     ldy #6
 :
@@ -194,10 +181,8 @@ again:
     ldx column
     lda #32
     sta bitmapcharstart,x
-   
 sm: lda #0 ; health
     beq :+
-   
     dec meltCount
     bne again
     rts
@@ -207,134 +192,124 @@ sm: lda #0 ; health
     and #$01 ; KEY_RETURN (0x80 on VIC)
     beq again
     rts
-
 .endproc
 
+.segment "LOWCODE2"
+
+faceColor:      .byte 7,3
+faceChars:      .byte 27,28,35,36,42,43
+faceOff:        .byte 0,1,40,41,80,81
+             ;; .byte $c2,$c3,$d8,$d9,$ee,$ef	
+changeLookTime: .byte 7
+lookDir:        .byte 0
+
+.proc _colorFace: near
+    ; A = godMode 0 or 1
+    ; convert to yellow (7) or cyan (3)
+    tay
+    lda faceColor,y
+    ldx #5
+  :
+    ldy faceOff,x
+    sta facecolorstart,y
+    dex
+    bpl :-
+    rts
+.endproc
+
+.proc _drawFace: near
+    ldx #5
+  :
+    ldy faceOff,x
+    lda faceChars,x
+    sta facestart,y
+    dex
+    bpl :-
+    rts
+.endproc
+
+.proc _updateFace: near
+    dec changeLookTime
+    beq :+
+    rts
+  :
+    lda lookDir
+    eor #$ff
+    sta lookDir
+    beq :+
+    ldx #6
+    ldy #40
+    bne drawFacePart
+  :
+    ldx #12
+    ldy #35
+drawFacePart:
+    stx changeLookTime
+    sty facestart+40
+    iny
+    sty facestart+41
+    rts
+.endproc
 
 .segment "LOWCODE2"
 .proc _print3DigitNumToScreen: near
-  ; AX pos
-  ; TOS num (char)
+    ; AX pos
+    ; TOS num (char)
+    sta sm1+1
+    sta sm2+1
+    txa
+    sta sm1+2
+    clc
+    adc #(charcolorstart-charscreenstart)/256
+    sta sm2+2
 
-  sta sm1+1
-  sta sm2+1
-  txa
-  sta sm1+2
-  clc
-  adc #(charcolorstart-charscreenstart)/256
-  sta sm2+2
+    ldy #0
+    lda (sp),y
+    jsr intToAsc
+    pha
+    txa
+    pha
+    tya
+    pha
 
-  ldy #0
-  lda (sp),y
-  jsr intToAsc
-  pha
-  txa
-  pha
-  tya
-  pha
-
-  ldy #0
+    ldy #0
   :
-  pla
+    pla
 sm1: sta $0400,y
-  lda textcolor
+    lda textcolor
 sm2: sta $d800,y
-  iny
-  cpy #3
-  bne :-
+    iny
+    cpy #3
+    bne :-
 
-  jmp incsp1
-
+    jmp incsp1
 .endproc
-
 
 .proc _print2DigitNumToScreen: near
-  ; AX pos
-  ; TOS num (char)
+    ; AX pos
+    ; TOS num (char)
+    sta sm1+1
+    sta sm2+1
+    txa
+    sta sm1+2
+    clc
+    adc #(charcolorstart-charscreenstart)/256
+    sta sm2+2
+    ldy #0
+    lda (sp),y
+    jsr intToAsc
+    pha
+    txa
+    pha
 
-  sta sm1+1
-  sta sm2+1
-  txa
-  sta sm1+2
-  clc
-  adc #(charcolorstart-charscreenstart)/256
-  sta sm2+2
-
-  ldy #0
-  lda (sp),y
-  jsr intToAsc
-  pha
-  txa
-  pha
-
-  ldy #0
+    ldy #0
   :
-  pla
+    pla
 sm1: sta $0400,y
-  lda textcolor
+    lda textcolor
 sm2: sta $d800,y
-  iny
-  cpy #2
-  bne :-
-
-  jmp incsp1
-
+    iny
+    cpy #2
+    bne :-
+    jmp incsp1
 .endproc
-
-
-faceColor:
-.byte 7,3
-faceChars:
-.byte 27,28,35,36,42,43
-faceOff:
-.byte 0,1,40,41,80,81
-;; .byte $c2,$c3,$d8,$d9,$ee,$ef	
-changeLookTime:
-.byte 7
-lookDir:
-.byte 0
-
-_colorFace:
-  ; A = godMode 0 or 1
-  ; convert to yellow (7) or cyan (3)
-  tay
-  lda faceColor,y
-  ldx #5
-  :
-  ldy faceOff,x
-  sta facecolorstart,y
-  dex
-  bpl :-
-  rts
-
-_drawFace:
-  ldx #5
-  :
-  ldy faceOff,x
-  lda faceChars,x
-  sta facestart,y
-  dex
-  bpl :-
-  rts
-
-_updateFace:
-  dec changeLookTime
-  beq :+
-  rts
-  :
-  lda lookDir
-  eor #$ff
-  sta lookDir
-  beq :+
-  ldx #6
-  ldy #40
-  bne drawFacePart
-  :
-  ldx #12
-  ldy #35
-drawFacePart:
-  stx changeLookTime
-  sty facestart+40
-  iny
-  sty facestart+41
-  rts
